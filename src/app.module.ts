29717b3cfb7debbfb45ffdbfd2a1997a6bef95f2
Adedeji Oluwaobi <oluwatobiisaiah0409@gmail.com>
2023-07-12 18:56:16 +0100

    First Commit

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal:true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    TypeOrmModule.forRoot({
+      type:"mysql",
+      host:process.env.DB_HOST,
+      port:3306,
+      username:process.env.DB_USER,
+      password:process.env.DB_PASS,
+      database:process.env.DB_NAME, //database name replace with your database name but it should be same as this name
+      entities:[__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize:true,
+
+    })
+  ]
+})
+export class AppModule {}
