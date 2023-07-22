8313d4c7bda78e763fa692f69de1bc45d28bac6d
ugo <ndujesco@users.noreply.github.com>
2023-07-22 11:21:56 +0100

    make corrections to Jwt configuration

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -5,17 +5,28 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     PassportModule.register({
       defaultStrategy: ['jwt'],
     }),
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: {
-        expiresIn: 7200,
-      },
+    // JwtModule.register({
+    //   secret: process.env.JWT_SECRET, // this did not work because the .env filr hadn't yet been read
+    //   signOptions: {
+    //     expiresIn: 7200,
+    //   },
+    // }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: 7200,
+        },
+      }),
+      inject: [ConfigService],
     }),
     TypeOrmModule.forFeature([User]),
   ],
