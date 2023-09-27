ce5dd8b6d6988d02621ea5f48767c471a0c5d2ca
ugo <ndujesco@users.noreply.github.com>
2023-09-27 12:56:56 +0100

    update

--- src/modules/image_upload/image_upload.resolver.spec.ts
+++ src/modules/image_upload/image_upload.resolver.spec.ts
@@ -2,12 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ImageUploadResolver } from './image_upload.resolver';
 import { ImageUploadService } from './image_upload.service';
 
+const mockImageUploadService = () => ({});
+
 describe('ImageUploadResolver', () => {
   let resolver: ImageUploadResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ImageUploadResolver, ImageUploadService],
+      providers: [
+        ImageUploadResolver,
+        { provide: ImageUploadService, useFactory: mockImageUploadService },
+      ],
     }).compile();
 
     resolver = module.get<ImageUploadResolver>(ImageUploadResolver);
--- src/modules/image_upload/image_upload.service.spec.ts
+++ src/modules/image_upload/image_upload.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ImageUploadService } from './image_upload.service';
+import { FileUploadService } from 'src/file_upload/file_upload.interface';
+import { HuelagerRepository } from '../huelager/huelager.repository';
+import { ProductRepository } from '../product/product.repository';
+
+const mockFileUploadService = () => ({});
+const mockHuelagerRepository = () => ({});
+const mockProductRepository = () => ({});
 
 describe('ImageUploadService', () => {
   let service: ImageUploadService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ImageUploadService],
+      providers: [
+        ImageUploadService,
+        { provide: FileUploadService, useFactory: mockFileUploadService },
+        { provide: HuelagerRepository, useFactory: mockHuelagerRepository },
+        { provide: ProductRepository, useFactory: mockProductRepository },
+      ],
     }).compile();
 
     service = module.get<ImageUploadService>(ImageUploadService);
