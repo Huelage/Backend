eb59c27cb5e59841d1f0e18e139d4ab8074846e7
ugo <ndujesco@users.noreply.github.com>
2023-09-28 16:55:25 +0100

    update

--- src/modules/huelager/huelager.resolver.spec.ts
+++ src/modules/huelager/huelager.resolver.spec.ts
@@ -1,6 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HuelagerResolver } from './huelager.resolver';
 import { HuelagerService } from './huelager.service';
+import { DeepMocked } from '@golevelup/ts-jest';
+import { Huelager } from './entities/huelager.entity';
+import { UpdatePhoneInput } from './dtos/update-phone.input';
+import { VerifyPhoneInput } from './dtos/verify-phone.input';
+import { VerifyEmailInput } from './dtos/verify-email.input';
+import { ForgotPasswordInput } from './dtos/forgot-password.input';
+import { UpdatePasswordInput } from './dtos/update-password.input';
 
 const mockHuelagerService = () => ({
   refreshToken: jest.fn(),
@@ -14,8 +21,8 @@ const mockHuelagerService = () => ({
 });
 
 describe('HuelagerResolver', () => {
-  let resolver;
-  let service;
+  let resolver: HuelagerResolver;
+  let service: DeepMocked<HuelagerService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -26,7 +33,7 @@ describe('HuelagerResolver', () => {
     }).compile();
 
     resolver = module.get<HuelagerResolver>(HuelagerResolver);
-    service = module.get<HuelagerService>(HuelagerService);
+    service = module.get(HuelagerService);
   });
 
   it('should be defined', () => {
@@ -40,92 +47,113 @@ describe('HuelagerResolver', () => {
   });
 
   describe('refreshAccessToken', () => {
+    const huelager = new Huelager();
+
     it('calls the refreshToken service; refreshes the access token and returns it.', async () => {
       service.refreshToken.mockResolvedValue('testToken');
 
-      const result = await resolver.refreshAccessToken({ user: 'mockUser' });
+      const result = await resolver.refreshAccessToken({ user: huelager });
 
-      expect(service.refreshToken).toHaveBeenCalledWith('mockUser');
+      expect(service.refreshToken).toHaveBeenCalledWith(huelager);
       expect(result).toEqual('testToken');
     });
   });
 
   describe('updatePhone', () => {
+    const huelager = new Huelager();
+    const updatePhoneInput = new UpdatePhoneInput();
+
     it('calls the updatePhone service; updates the hulagers phone number and returns the huelager.', async () => {
-      service.updatePhone.mockResolvedValue('mockHuelager');
+      service.updatePhone.mockResolvedValue(huelager);
 
-      const result = await resolver.updatePhone('mockInPut');
+      const result = await resolver.updatePhone(updatePhoneInput);
 
-      expect(service.updatePhone).toHaveBeenCalledWith('mockInPut');
-      expect(result).toEqual('mockHuelager');
+      expect(service.updatePhone).toHaveBeenCalledWith(updatePhoneInput);
+      expect(result).toEqual(huelager);
     });
   });
 
   describe('verifyPhoneOtp', () => {
+    const huelager = new Huelager();
+    const verifyPhoneInput = new VerifyPhoneInput();
+
     it('calls the verifyPhone service; verifies the huelager otp and returns the huelager.', async () => {
-      service.verifyPhone.mockResolvedValue('mockHuelager');
+      service.verifyPhone.mockResolvedValue(huelager);
 
-      const result = await resolver.verifyPhoneOtp('mockInPut');
+      const result = await resolver.verifyPhoneOtp(verifyPhoneInput);
 
-      expect(service.verifyPhone).toHaveBeenCalledWith('mockInPut');
-      expect(result).toEqual('mockHuelager');
+      expect(service.verifyPhone).toHaveBeenCalledWith(verifyPhoneInput);
+      expect(result).toEqual(huelager);
     });
   });
 
   describe('requestEmailVerification', () => {
-    it('calls the requestEmailVerification service; requests the email verification and returns the huelager.', async () => {
-      service.requestEmailVerification.mockResolvedValue('mockHuelager');
+    const huelager = new Huelager();
 
-      const result = await resolver.requestEmailVerification('mockInPut');
+    it('calls the requestEmailVerification service; requests the email verification and returns the huelager.', async () => {
+      service.requestEmailVerification.mockResolvedValue(huelager);
+
+      const result = await resolver.requestEmailVerification('testEmail');
 
       expect(service.requestEmailVerification).toHaveBeenCalledWith(
-        'mockInPut',
+        'testEmail',
       );
-      expect(result).toEqual('mockHuelager');
+      expect(result).toEqual(huelager);
     });
   });
 
   describe('verifyEmailOtp', () => {
+    const huelager = new Huelager();
+    const verifyEmailInput = new VerifyEmailInput();
+
     it('calls the verifyEmail service; verifies the huelager email and returns the huelager.', async () => {
-      service.verifyEmail.mockResolvedValue('mockHuelager');
+      service.verifyEmail.mockResolvedValue(huelager);
 
-      const result = await resolver.verifyEmailOtp('mockInPut');
+      const result = await resolver.verifyEmailOtp(verifyEmailInput);
 
-      expect(service.verifyEmail).toHaveBeenCalledWith('mockInPut');
-      expect(result).toEqual('mockHuelager');
+      expect(service.verifyEmail).toHaveBeenCalledWith(verifyEmailInput);
+      expect(result).toEqual(huelager);
     });
   });
 
   describe('forgotPassword', () => {
+    const huelager = new Huelager();
+    const forgotPasswordInput = new ForgotPasswordInput();
+
     it('calls the forgotPassword service; updates the password and returns the huelager.', async () => {
-      service.forgotPassword.mockResolvedValue('mockHuelager');
+      service.forgotPassword.mockResolvedValue(huelager);
 
-      const result = await resolver.forgotPassword('mockInPut');
+      const result = await resolver.forgotPassword(forgotPasswordInput);
 
-      expect(service.forgotPassword).toHaveBeenCalledWith('mockInPut');
-      expect(result).toEqual('mockHuelager');
+      expect(service.forgotPassword).toHaveBeenCalledWith(forgotPasswordInput);
+      expect(result).toEqual(huelager);
     });
   });
 
   describe('updatePassword', () => {
+    const huelager = new Huelager();
+    const updatePasswordInput = new UpdatePasswordInput();
+
     it('calls the updatePassword service; updates the password and returns the huelager.', async () => {
-      service.updatePassword.mockResolvedValue('mockHuelager');
+      service.updatePassword.mockResolvedValue(huelager);
 
-      const result = await resolver.updatePassword('mockInPut');
+      const result = await resolver.updatePassword(updatePasswordInput);
 
-      expect(service.updatePassword).toHaveBeenCalledWith('mockInPut');
-      expect(result).toEqual('mockHuelager');
+      expect(service.updatePassword).toHaveBeenCalledWith(updatePasswordInput);
+      expect(result).toEqual(huelager);
     });
   });
 
   describe('generateRSAKey', () => {
+    const huelager = new Huelager();
+
     it('calls the generateRSAKey service; generate the an RSA key pair and returns the public key.', async () => {
-      service.generateRSAKey.mockResolvedValue('mockHuelager');
+      service.generateRSAKey.mockResolvedValue('publicKey');
 
-      const result = await resolver.generateRSAKey({ user: 'mockUser' });
+      const result = await resolver.generateRSAKey({ user: huelager });
 
-      expect(service.generateRSAKey).toHaveBeenCalledWith('mockUser');
-      expect(result).toEqual('mockHuelager');
+      expect(service.generateRSAKey).toHaveBeenCalledWith(huelager);
+      expect(result).toEqual('publicKey');
     });
   });
 });
