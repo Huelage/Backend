71c8e9a01645cc30a6c3376d1da2c3a5eda62926
ugo <ndujesco@users.noreply.github.com>
2023-09-28 10:36:23 +0100

    update

--- src/modules/huelager/user/user.resolver.spec.ts
+++ src/modules/huelager/user/user.resolver.spec.ts
@@ -3,10 +3,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
 
-const mockUserService = () => ({});
+const mockUserService = () => ({
+  create: jest.fn(),
+  signIn: jest.fn(),
+});
 
 describe('UserResolver', () => {
-  let resolver: UserResolver;
+  let resolver;
+  let service;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -17,9 +21,32 @@ describe('UserResolver', () => {
     }).compile();
 
     resolver = module.get<UserResolver>(UserResolver);
+    service = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  describe('signUpUser', () => {
+    it('calls the create service; creates a user and returns the user ', async () => {
+      service.create.mockResolvedValue('mockUser');
+
+      const result = await resolver.signUpUser('mockInput');
+
+      expect(service.create).toHaveBeenCalledWith('mockInput');
+      expect(result).toEqual('mockUser');
+    });
+  });
+
+  describe('signInUser', () => {
+    it('calls the signIn service; signs in a user and returns the user ', async () => {
+      service.signIn.mockResolvedValue('mockUser');
+
+      const result = await resolver.signInUser('mockInput');
+
+      expect(service.signIn).toHaveBeenCalledWith('mockInput');
+      expect(result).toEqual('mockUser');
+    });
   });
 });
--- src/modules/huelager/vendor/vendor.resolver.spec.ts
+++ src/modules/huelager/vendor/vendor.resolver.spec.ts
@@ -3,10 +3,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { VendorResolver } from './vendor.resolver';
 import { VendorService } from './vendor.service';
 
-const mockVendorService = () => ({});
+const mockVendorService = () => ({
+  create: jest.fn(),
+  signIn: jest.fn(),
+});
 
 describe('VendorResolver', () => {
-  let resolver: VendorResolver;
+  let resolver;
+  let service;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -17,9 +21,32 @@ describe('VendorResolver', () => {
     }).compile();
 
     resolver = module.get<VendorResolver>(VendorResolver);
+    service = module.get<VendorService>(VendorService);
   });
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  describe('signUpVendor', () => {
+    it('calls the create service; creates a vendor and returns the vendor ', async () => {
+      service.create.mockResolvedValue('mockVendor');
+
+      const result = await resolver.signUpVendor('mockInput');
+
+      expect(service.create).toHaveBeenCalledWith('mockInput');
+      expect(result).toEqual('mockVendor');
+    });
+  });
+
+  describe('signInVendor', () => {
+    it('calls the signIn service; signs in a vendor and returns the vendor ', async () => {
+      service.signIn.mockResolvedValue('mockVendor');
+
+      const result = await resolver.signInVendor('mockInput');
+
+      expect(service.signIn).toHaveBeenCalledWith('mockInput');
+      expect(result).toEqual('mockVendor');
+    });
   });
 });
