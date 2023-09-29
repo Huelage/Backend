683bfd649736b4a7cbaef1466ba518232b5615c7
ugo <ndujesco@users.noreply.github.com>
2023-09-30 00:33:41 +0100

    add config

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import * as graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js';
+import * as config from 'config';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+  const serverConfig = config.get('server');
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -18,7 +21,8 @@ async function bootstrap() {
     graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 10 }),
   );
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || serverConfig.port;
+
   await app.listen(port);
 
   logger.verbose(
